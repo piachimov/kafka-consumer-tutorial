d7ccee930d5dbedd4940ee48fd9567d3b359418a
piachimov <72208903+piachimov@users.noreply.github.com>
2021-10-15 07:50:58 +0300

    first commit

---
+++ src/main/java/com/kafka/tutorial/listener/KafkaConsumer.java
@@ -0,0 +1,28 @@
+package com.kafka.tutorial.listener;
+
+import com.kafka.tutorial.model.User;
+import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.stereotype.Service;
+
+@Service
+public class KafkaConsumer {
+
+    @KafkaListener(topics = "Kafka_Example", groupId = "group_id")
+    public void consume(String message) {
+        System.out.println("Consumed message: " + message);
+    }
+
+
+    @KafkaListener(topics = "Kafka_Example_json", groupId = "group_json",
+            containerFactory = "userKafkaListenerFactory")
+    public void consumeJson(User user) {
+        System.out.println("Consumed JSON Message: " + user);
+        validatePermissions(user);
+    }
+
+    private void validatePermissions(User user) {
+        if (user.getName() == null){
+
+        }
+    }
+}
